6367e182672acd7c375bc8e7a46632503b104064
TLKoch <Tiziana.Li@t-online.de>
2019-05-27 15:04:26 +0200

    final solution

--- main.py
+++ main.py
@@ -1,20 +1,32 @@
 # Exercise 3
 from pathlib import Path
+
+basedir = Path("C:/Users/Kochti/Desktop/Studium/Python/exercise-3-TLKoch/")
+data_dir = basedir / "data"
+output_dir = basedir / "solution"
+
 # import functions from utils here
+import utils as ut
 
-data_dir = Path("data")
-output_dir = Path("solution")
+# 1. Construct the path to the text file in the data directory using the `pathlib` module [2P]
 
-# 1. Contstruct the path to the text file in the data directory using the `pathlib` module [2P]
-
+cars_path = data_dir / "cars.txt"
+print(cars_path)
 
 # 2. Read the text file [2P]
 
+with open(cars_path, "r") as file:
+    content = file.read().splitlines()
 
 # 3. Count the occurences of each item in the text file [2P]
-
+ 
+# possible function for this tasks, see an all_in_one solution in task 5    
+# ut.countocc(content)    
 
 # 4. Using `pathlib` check if a directory with name `solution` exists and if not create it [2P]
+
+# possible function for this tasks, see an all_in_one solution in task 5  
+# ut.directorycheck(output_dir)
 
 
 # 5. Write the counts to the file `counts.csv` in the `solution` directory in the format (first line is the header): [2P]
@@ -22,3 +34,10 @@ output_dir = Path("solution")
 #    item_name_1, item_count_1
 #    item_name_2, item_count_2
 #    ...
+
+# Actually it is easier to combine the two previous tasks with this last one to one great function, which only needs the txt-Path and an output-directory as input. 
+ut.all_in_one(content, output_dir)
+
+
+        
+        
--- utils.py
+++ utils.py
@@ -2,3 +2,88 @@
 
 # Use this file for all functions you create and might want to reuse later.
 # Import them in the `main.py` script
+
+
+# possible function for task 3, see an all_in_one solution below in the all_in_one Function 
+def countocc(x):
+    """
+    Calculate the occurence of each item in a text file.
+    
+    Parameters
+    ----------
+    x : text file
+        
+    Returns
+    -------
+    item and the corresponding number of occurence of this item in the text file
+    
+    """
+    
+    items = list(set(x))
+    for value in items: 
+        print(value, x.count(value))
+        
+        
+           
+# possible function for task 4, see an all_in_one solution below in the all_in_one Function 
+def directorycheck(output):
+    """
+    Checks, if a directory exists. If it does not exist, it creates this directory.
+    
+    Parameters
+    ----------
+    output : directory
+        
+    Returns
+    -------
+    Prints a statement whether the given directory already existed or whether it had to be created
+    
+    """
+    temp = output.exists()
+    if temp == True:
+        print("Your results will be saved into the directory \"{}\" ".format(output))
+    else:    
+        output.mkdir()
+        print("Unfortunately your directory \"{}\" did not exist, so it was created. Your results will be saved in there".format(output))
+    
+            
+        
+# Function for creating a csv file in a directory for the item and its counts        
+def all_in_one(x, output):
+    """
+    Calculate the occurence of each item in a text file. Checks, if a output directory exists. If it does not exist, it creates this directory and finally saves 
+    the results there in a csv file
+    
+    Parameters
+    ----------
+    x : text file
+    output: an output directory, where to save the results
+        
+    Returns
+    -------
+    A csv file with each item from a textfile and the corresponding number of occurence of this item in the text file
+    
+    """
+    import pandas as pd
+    # Create dataframe for counting the occurences for each item in a text file 
+    items = list(set(x))
+    countings = []
+    for value in items: 
+        countings.append(x.count(value))
+    countingsdf = pd.DataFrame({'model': items, 'countings': countings})
+    
+    # Checking output directory
+    temp = output.exists()
+    if temp == True:
+        print("Your results are saved into the directory \"{}\" ".format(output))
+    else:    
+        output.mkdir()
+        print("Unfortunately your directory \"{}\" did not exist, so it was created. Your results are saved in there".format(output))
+        
+        
+    # savind the results into a csv file
+    output_name = output / "results_counting_occurences.csv"      
+    countingsdf.to_csv (output_name, header = True)
+
+      
+                
